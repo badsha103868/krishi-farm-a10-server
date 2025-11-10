e6531807ccd108b352217824e05d837f89c01b9d
badsha103868 <badshagolder5@gmail.com>
2025-11-10 12:54:45 +0600

    added connected to mongoDB

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 const express = require('express');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.port || 3000;
 
 
 
+
+
+
+
 // middleware
 app.use(cors());
 app.use(express.json())
+
+
+
+// mongodb uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@portfolio-cluster1.ea8n2bl.mongodb.net/?appName=portfolio-cluster1`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+ async function run(){
+  try{
+     await client.connect();
+     await client.db('admin').command({ping : 1})
+     console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  }
+  finally{
+
+  }
+ }
+ run().catch(console.dir)
+
 
 
 // normal get
