9246c9823b16181bdf5ffea4bd0e8b6e29eefdcc
badsha103868 <badshagolder5@gmail.com>
2025-11-13 02:34:50 +0600

    added crop details  api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -74,7 +74,14 @@ const client = new MongoClient(uri, {
       const result = await cursor.toArray();
       res.send(result);
     }) 
-
+    
+    //  single crop find
+    app.get('/crops/:id', async (req, res)=>{
+       const id = req.params.id;
+       const query = { _id : new ObjectId(id)}
+       const result = await cropsCollection.findOne(query);
+       res.send(result)
+    })
 
 
   
